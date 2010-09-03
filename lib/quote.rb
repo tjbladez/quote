1c3521dcd6acfe5da087d33841b0a06238a3660f
Nickolay Schwarz <tjbladez@gmail.com>
2010-09-03 10:29:32 -0500

    Changing quote

--- lib/quote.rb
+++ lib/quote.rb
@@ -3,20 +3,26 @@ require 'colored'
 module Quote
   def self.say(source = nil)
     quotes = Yajl::Parser.parse(File.new(File.dirname(__FILE__) + '/quotes.json'))
-    if source
-      matching_quotes = quotes.values.flatten.select do |hash|
-        hash["source"].match(/#{source}/i)
-      end
-      entry = random_select(matching_quotes)
-      "\"#{entry['quote'].green}\" - #{entry['actor'].red}, #{entry['source'].red}."
+    if source && !find_matches(quotes, source).empty?
+      quote(random_select(find_matches(quotes, source)))
     else
       category = random_select(quotes.to_a)
       entry    = random_select(category.last)
-      "#{category.first.capitalize}: \"#{entry['quote'].green}\" - #{entry['actor'].red}, #{entry['source'].red}."
+      "#{category.first.capitalize}: " + quote(entry)
     end
   end
 
   def self.random_select(enumerable)
     enumerable[rand(enumerable.size)]
+  end
+
+  def self.find_matches(quotes, source)
+    quotes.values.flatten.select do |hash|
+      hash["source"].match(/#{source}/i)
+    end
+  end
+
+  def self.quote(entry)
+    "\"#{entry['quote'].green}\" - #{entry['actor'].red}, #{entry['source'].red}."
   end
 end

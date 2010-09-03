edaf027dca758a747a8a4d68fee3bc87e66337c9
Nickolay Schwarz <tjbladez@gmail.com>
2010-09-03 14:00:29 -0500

    Adding support for searching for multiple criteria

--- lib/quote.rb
+++ lib/quote.rb
@@ -1,28 +1,31 @@
 require 'yajl'
 require 'colored'
-module Quote
-  def self.say(source = nil)
-    quotes = Yajl::Parser.parse(File.new(File.dirname(__FILE__) + '/quotes.json'))
-    if source && !find_matches(quotes, source).empty?
-      quote(random_select(find_matches(quotes, source)))
-    else
-      category = random_select(quotes.to_a)
-      entry    = random_select(category.last)
-      "#{category.first.capitalize}: " + quote(entry)
+require 'ruby-debug'
+
+module ArrayExtensions
+  def where(criteria ={})
+    result = self
+    criteria.each do |cr_key, cr_value|
+      result = result.select do |hash|
+        hash[cr_key].to_s.match(/#{cr_value}/i)
+      end
     end
+    result
+  end
+end
+Array.send(:include, ArrayExtensions)
+
+module Quote
+  def self.say(criteria = {})
+    quotes = Yajl::Parser.parse(File.new(File.dirname(__FILE__) + '/quotes.json'))
+    quote(random_select(quotes.where(criteria)))
   end
 
   def self.random_select(enumerable)
     enumerable[rand(enumerable.size)]
   end
 
-  def self.find_matches(quotes, source)
-    quotes.values.flatten.select do |hash|
-      hash["source"].match(/#{source}/i)
-    end
-  end
-
   def self.quote(entry)
-    "\"#{entry['quote'].green}\" - #{entry['actor'].red}, #{entry['source'].red}."
+    "#{entry['theme']}: \"#{entry['quote'].green}\" - #{entry['source'].red}, #{entry['context'].red}."
   end
 end
